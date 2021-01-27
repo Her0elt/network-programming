fe53dc3b7c82959de98cfe0e3f37135436214c80
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2021-01-27 17:29:51 +0100

    clean up

--- exercise2/AsyncSocketServer.kt
+++ exercise2/AsyncSocketServer.kt
@@ -1,7 +1,6 @@
 package exercise2
 
 import java.net.InetSocketAddress
-import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 
 suspend fun runServer(){
@@ -9,12 +8,8 @@ suspend fun runServer(){
     server.asyncBind(InetSocketAddress("127.0.0.1", 1250))
     val worker: AsynchronousSocketChannel =  server.asyncAccept()
     val instructions = "Write a equation in the format 1 + 1, then i'll give you the answer, end with enter"
-    worker.asyncWrite(ByteBuffer.wrap(instructions.toByteArray()))
-    val buffer: ByteBuffer = ByteBuffer.allocate(128)
-    worker.asyncRead(buffer)
-    var msg = String(buffer.array()).trim { it <= ' ' }
-    println(msg)
-    buffer.clear()
+    sendMessage(instructions, worker)
+    var msg = readMessage(worker)
     while(msg != ""){
         val answer = calc(msg.split(" ".toRegex()).toTypedArray())
         sendMessage(answer, worker)
